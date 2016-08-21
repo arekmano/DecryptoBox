8423b69d818d274c95b4bb45619e8048d77114ed
Arek Mano <manoukian.arek@gmail.com>
2016-08-21 18:30:18 -0400

    added time to log and delete call after successful key/value fetch

--- index.js
+++ index.js
@@ -14,22 +14,55 @@ function init(){
 
 
 cron.schedule('* * * * * *', function(){
-  getEntries(decrypt);
+  getEntries(decrypt, removeEntries);
 });
 
-function getEntries(callback){
-    http.get(URL_OPTIONS, function(res) {
+function getEntries(decrypt_callback, remove_callback){
+  var get_options = {
+    hostname : URL_OPTIONS.hostname,
+    port : URL_OPTIONS.port,
+    agent : URL_OPTIONS.agent,
+    path: URL_OPTIONS.path
+  }
+    http.get(get_options, function(res) {
     res.on('data', function(chunk) {
-      callback(JSON.parse(chunk.toString())[0]);
+      var response_json = JSON.parse(chunk.toString());
+      if (response_json.length == 0){
+        console.log(new Date() + ': Found No entries');
+      } else {
+        console.log(new Date() + ': Found ' + response_json.length + ' pair(s)');
+        remove_callback();
+      }
+      response_json.forEach(function(element){
+        decrypt_callback(element);
+      })
     });
   });
 }
+
+function removeEntries(){
+  var delete_options = {
+    hostname : URL_OPTIONS.hostname,
+    port : URL_OPTIONS.port,
+    agent : URL_OPTIONS.agent,
+    method: 'DELETE',
+    path: URL_OPTIONS.path
+  }
+  var req = http.request(delete_options, function(res) {
+    res.on('data', function(chunk) {
+      console.log(new Date() + ': Cleared Messages.')
+    });
+  });
+  req.end();
+}
+
 
 function decrypt(pair){
   decrypted_pair = {
     key: crypto.privateDecrypt( {key: PRIVATE_KEY, padding: constants.RSA_PKCS1_PADDING}, new Buffer(pair.key, 'base64')).toString(),
     value: crypto.privateDecrypt( {key: PRIVATE_KEY, padding: constants.RSA_PKCS1_PADDING}, new Buffer(pair.value, 'base64')).toString()
   };
+  console.log(new Date() + ': Decrypted Key/Value');
   console.log(decrypted_pair);
   return decrypted_pair;
 }
